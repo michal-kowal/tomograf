52e7d86a72952a4c98100947172fd9d40410f605
michal-kowal <kowalewski.michal777@gmail.com>
2024-03-11 15:33:21 +0100

    add sinogram

--- simulation.py
+++ simulation.py
@@ -1,10 +1,21 @@
 from matplotlib.image import imread
 import numpy as np
 from skimage import color
-import brasenham
+from brasenham import bresenham_line
+import os
+import glob
+from PIL import Image
 
+
+def clear_directory(directory_path):
+    files = glob.glob(directory_path + '/*')
+    for f in files:
+        os.remove(f)
 
 def simulate(input_path, angle, detectors, span):
+    output_dir = "./result"
+    clear_directory(output_dir)
+    sinogram = np.zeros((int(2 * np.pi / angle), detectors))
     image = imread(input_path)
     try:
         image = color.rgb2gray(image)
@@ -19,3 +30,28 @@ def simulate(input_path, angle, detectors, span):
     angle = np.deg2rad(float(angle))
     span = np.deg2rad(float(span))
 
+    for e_id, e_angle in zip(range(int(2 * np.pi / angle)), np.arange(0, 2 * np.pi, angle)):
+        e_coords = [int (x + r * np.cos(e_angle)), int (y + r * np.sin(e_angle))]
+        
+        detector_first = e_angle + np.pi - span / 2
+        detector_last = e_angle + np.pi + span / 2
+        for d_id, d_angle in zip(range(detectors), np.arange(detector_first, detector_last, span / detectors)):
+            d_coords = [int (x + r * np.cos(e_angle + d_angle)), int (y + r * np.sin(e_angle + d_angle))]
+            line_points = bresenham_line(e_coords[0], e_coords[1], d_coords[0], d_coords[1])
+            points_counter = 0
+            sum = 0
+            for point in line_points: 
+                # sprawdź czy punkt leży w obrazie
+                if 0 <= point[0] < image.shape[0] and 0 <= point[1] < image.shape[1]:
+                    sum += image[point[0], point[1]]
+                    points_counter += 1
+            if points_counter > 0:
+                sinogram[e_id, d_id] = sum / points_counter
+            else:
+                sinogram[e_id, d_id] = 0
+    
+    sinogram_scaled = (255.0 / np.amax(sinogram)) * sinogram
+    sinogram_scaled = sinogram_scaled.astype(np.uint8)
+    sinogram_image = Image.fromarray(sinogram_scaled.T, mode='L')
+    sinogram_resized = sinogram_image.resize(image.shape, resample=Image.NEAREST)
+    sinogram_resized.save(output_dir + "/sinogram.png")
